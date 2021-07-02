f80c02b0f5a608f182a3cb5c288cac0bee6113fa
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-07-01 19:55:29 -0500

    finished basic html and css for a singe movie card

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// * make a function with variable of (renderMovies) and takes a parameter of (movieArray)
+const renderMovies = 
+// * write a .map() loop for (movieArray)
+// * while the function loops, the picked movie should be called (currentMovie)
+// * take the result of the (currentMovie) and save it to the variable of (movieHTMLArray)
+
+
+// todo ! make a function that .map() the array of all the movie details of data.js
+// todo ! variable used should be (movieArray)(renderMovies*function name)(currentMovie)(movieHTMLArray)
