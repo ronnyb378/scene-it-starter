7e1f885f4494dc6365465783e4b4fc872f915dcc
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-07-06 11:53:49 -0500

    everything is working ecapect for the button style/the event listener for button/& some css styling

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 
-// * make a function with variable of (renderMovies) and takes a parameter of (movieArray)
-const renderMovies = 
-// * write a .map() loop for (movieArray)
-// * while the function loops, the picked movie should be called (currentMovie)
-// * take the result of the (currentMovie) and save it to the variable of (movieHTMLArray)
+document.addEventListener('DOMContentLoaded', function () {
+    // * elements we need to create cards for each movie with eventlistener on the search bar
+    const container = document.querySelector('.container')
+    const bar = document.querySelector('.search')
+    const moviesContainer = document.querySelector('.movies-container')
+
+    // * event listener for the search bar
+    bar.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const searchBar = document.querySelector('.search-bar')
+        // console.log('test')
+        // * * this is the value of input at search
+        const movie = searchBar.value
+        // * maps through data.js array and sets variable of currentMovie
+        moviesContainer.innerHTML = movieData.map((currentMovie) => {
+            e.preventDefault()
+            const movieHtmlArray = currentMovie
+            return `<div class="card">
+                        <div class="image-fav">
+                            <img class="movie-picture" src="${movieHtmlArray.Poster}" alt="...">
+                            <button class="watchLater add-button" data-imdbid="${movieHtmlArray.imdbID}">Later?</button>
+                        </div>
+                        <div class="below-image">
+                        <div class="info-box title-date">
+                            <h5 class="card-title">${movieHtmlArray.Title}</h5>
+                            <h6 class="card-date">${movieHtmlArray.Year}</h6>
+                        </div>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores omnis quia, reiciendis facilis veritatis reprehenderit tempore officia dignissimos placeat fugit sapiente perspiciatis libero? Aut iure assumenda maiores, fugit at laudantium!</p>
+                        </div>
+                    </div>`
+        }).join('')
+    });
+    document.addEventListener('click', (e) => {
+        console.log('test')
+    })
+});
 
 
-// todo ! make a function that .map() the array of all the movie details of data.js
-// todo ! variable used should be (movieArray)(renderMovies*function name)(currentMovie)(movieHTMLArray)
